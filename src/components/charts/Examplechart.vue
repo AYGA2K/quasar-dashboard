0d1838055add79fe36602986b581b1a3017eaeda
AYGA2K <garouatayoub@gmail.com>
2022-10-11 03:43:00 +0100

    init

---
+++ src/components/charts/Examplechart.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <apexchart
+      type="bar"
+      height="350"
+      :options="chartOptions"
+      :series="series"
+    ></apexchart>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'myChart',
+  data() {
+    return {
+      series: [
+        {
+          data: [21, 22, 10, 28, 16, 21, 13, 30],
+        },
+      ],
+      chartOptions: {
+        chart: {
+          height: 350,
+          type: 'bar',
+          events: {
+            click: function (chart, w, e) {
+              console.log(chart, w, e);
+            },
+          },
+        },
+
+        plotOptions: {
+          bar: {
+            columnWidth: '45%',
+            distributed: true,
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        legend: {
+          show: false,
+        },
+        xaxis: {
+          categories: [
+            ['John', 'Doe'],
+            ['Joe', 'Smith'],
+            ['Jake', 'Williams'],
+            'Amber',
+            ['Peter', 'Brown'],
+            ['Mary', 'Evans'],
+            ['David', 'Wilson'],
+            ['Lily', 'Roberts'],
+          ],
+          labels: {
+            style: {
+              fontSize: '12px',
+            },
+          },
+        },
+      },
+    };
+  },
+};
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,222 @@
+<template>
+  <q-layout view="lHr lpR fFf" class="bg-primary">
+    <q-header class="row justify-center">
+      <div class="q-ma-sm bg-linear-secondary-primary border-radius-40px">
+        <q-input
+          rounded
+          borderless
+          v-model="text"
+          input-class="text-left text-white q-pa-sm "
+        >
+          <template v-slot:append>
+            <q-icon
+              class="q-pa-sm"
+              v-if="text === ''"
+              name="search"
+              color="positive"
+            />
+            <q-icon
+              v-else
+              name="clear"
+              @click="text = ''"
+              color="positive"
+              class="cursor-pointer q-pa-sm"
+            />
+          </template>
+        </q-input>
+      </div>
+    </q-header>
+
+    <q-drawer
+      side="left"
+      v-model="drawerLeft"
+      show-if-above
+      :mini="miniState"
+      :width="200"
+      :breakpoint="500"
+      class="bg-primary"
+    >
+      <div>
+        <q-icon
+          color="white"
+          name="menu"
+          class="q-ma-md cursor-pointer"
+          @click="miniState = !miniState"
+          size="md"
+        />
+      </div>
+
+      <q-list class="list-text">
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="inbox" />
+          </q-item-section>
+
+          <q-item-section> Inbox </q-item-section>
+        </q-item>
+
+        <q-item active clickable v-ripple active-class="my-active-class">
+          <q-item-section avatar>
+            <q-icon name="star" />
+          </q-item-section>
+
+          <q-item-section> Star </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="send" />
+          </q-item-section>
+
+          <q-item-section> Send </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="drafts" />
+          </q-item-section>
+
+          <q-item-section> Drafts </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+    <q-drawer
+      side="right"
+      v-model="drawerRight"
+      show-if-above
+      :width="400"
+      :breakpoint="500"
+      class="bg-primary"
+    >
+      <q-list class="bg-secondary q-ma-md q-pa-md border-radius-15px">
+        <div class="row justify-end items-center">
+          <q-icon
+            class="q-mx-sm"
+            size="sm"
+            color="white"
+            name="notifications"
+          />
+          <q-avatar class="q-mx-md" size="md">
+            <img src="https://cdn.quasar.dev/img/avatar.png" />
+          </q-avatar>
+        </div>
+        <div class="q-mx-sm">
+          <p class="text-white text-bold">Recent Orders</p>
+          <div class="row list-text justify-between text-subtitle2">
+            <p>Orders</p>
+            <p class="q-px-lg">Amount</p>
+          </div>
+          <div class="q-gutter-y-md">
+            <div class="row list-text justify-between">
+              <q-avatar size="lg" square>
+                <img src="https://cdn.quasar.dev/img/avatar.png" />
+              </q-avatar>
+              <div>
+                <div class="text-white">Thrsea Web</div>
+                <div class="text-1rem">2 minutes ago</div>
+              </div>
+              <p
+                class="text-white bg-primary q-px-lg q-py-sm border-radius-10px"
+              >
+                1000$
+              </p>
+            </div>
+            <div class="row list-text justify-between">
+              <q-avatar size="lg" square>
+                <img src="https://cdn.quasar.dev/img/avatar.png" />
+              </q-avatar>
+              <div>
+                <div class="text-white">Thrsea Web</div>
+                <div class="text-1rem">2 minutes ago</div>
+              </div>
+              <p
+                class="text-white bg-primary q-px-lg q-py-sm border-radius-10px"
+              >
+                1000$
+              </p>
+            </div>
+            <div class="row list-text justify-between">
+              <q-avatar size="lg" square>
+                <img src="https://cdn.quasar.dev/img/avatar.png" />
+              </q-avatar>
+              <div>
+                <div class="text-white">Thrsea Web</div>
+                <div class="text-1rem">2 minutes ago</div>
+              </div>
+              <p
+                class="text-white bg-primary q-px-lg q-py-sm border-radius-10px"
+              >
+                1000$
+              </p>
+            </div>
+          </div>
+        </div>
+      </q-list>
+      <div class="bg-secondary q-ma-md q-pa-md border-radius-15px">
+        <ApexChart></ApexChart>
+      </div>
+    </q-drawer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineAsyncComponent, defineComponent } from 'vue';
+
+const ApexChart = defineAsyncComponent(
+  () => import('src/components/charts/Examplechart.vue')
+);
+export default defineComponent({
+  components: {
+    ApexChart,
+  },
+  data(): {
+    miniState: boolean;
+    text: string;
+    drawerLeft: boolean;
+    drawerRight: boolean;
+  } {
+    return {
+      miniState: false,
+      text: '',
+      drawerLeft: false,
+      drawerRight: false,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.bg-linear-secondary-primary {
+  background: linear-gradient(144deg, rgba(21, 46, 95, 1) 20%, $primary);
+}
+.border-radius-40px {
+  border-radius: 40px;
+  border: 2px;
+}
+.border-radius-15px {
+  border-radius: 15px;
+  border: 2px;
+}
+.border-radius-10px {
+  border-radius: 10px;
+  border: 2px;
+}
+.my-active-class {
+  background: linear-gradient(
+    90deg,
+    rgba(146, 103, 255, 1) 10%,
+    rgba(146, 103, 255, 0) 100%
+  );
+  color: white;
+}
+.list-text {
+  color: $menuItem;
+  font-weight: bold;
+}
+.text-1rem {
+  font-size: x-small;
+}
+</style>
